6828e523778fad172a0ec8f58bd1e88f6b102ccc
Jim <jimhardison36@yahoo.com>
2018-07-03 10:55:40 -0700

    Fix carousel

---
+++ resources/js/main.js
@@ -0,0 +1,13 @@
+   $(document).ready(function(){
+     $('.carousel').slick({
+       setting-name: setting-value
+       autoPlay: true;
+       autoPlaySpeed: 4000;
+       arrows: false;
+       draggable: false;
+       pauseOnFocus: false;
+       pauseOnHover: false;
+       slidesToScroll: 3;
+       swipe: false;
+     });
+   });
