eb3874c92a08dc2c18ef6949ccbcf85d28b12bd0
Jim <jimhardison36@yahoo.com>
2018-07-06 14:26:42 -0700

    Fix errors in javascript.

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,25 +1,24 @@
    $(document).ready(function(){
      $('.carousel').slick({
-       setting-name: setting-value
-       autoPlay: true;
-       autoPlaySpeed: 4000;
-       arrows: false;
-       draggable: false;
-       pauseOnFocus: false;
-       pauseOnHover: false;
-       slidesToScroll: 3;
-       swipe: false;
+       autoPlay: true,
+       autoPlaySpeed: 4000,
+       arrows: false,
+       draggable: false,
+       pauseOnFocus: false,
+       pauseOnHover: false,
+       slidesToScroll: 1,
+       swipe: false
      });
      var lastScrollTop = 0;
-   $(window).scroll(function(){
-     var scrollTop = $(this).scrollTop();
-     if (scrollTop - lastScrollTop > 50){
-       var navHeight = $('.navbar').css('height');
-       $('.navbar').animate({top: '-' + navHeight}, 150);
-       lastScrollTop = scrollTop;
-     } else if (lastScrollTop - scrollTop > 50) {
-       $('.navbar').animate({top: '0px'}, 150);
-       lastScrollTop = scrollTop;
-     }
+     $(window).scroll(function(){
+       var scrollTop = $(this).scrollTop();
+       if (scrollTop - lastScrollTop > 50){
+         var navHeight = $('.navbar').css('height');
+         $('.navbar').animate({top: '-' + navHeight}, 150);
+         lastScrollTop = scrollTop;
+       } else if (lastScrollTop - scrollTop > 50) {
+         $('.navbar').animate({top: '0px'}, 150);
+         lastScrollTop = scrollTop;
+       }
    });
  });
